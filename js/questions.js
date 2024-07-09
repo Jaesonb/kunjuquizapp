16b7f9bdbf81402e2970902273f16fa5cf6e12f2
Jaesonb <42680558+Jaesonb@users.noreply.github.com>
2024-07-09 12:06:29 +0530

    Add files via upload
    v3

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Friday variya?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "Maybe Yes",
+      "No, but pakla",
+      "pakla"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Nee eppa taa free?",
+    answer: "This Friday",
+    options: [
+      "This Friday",
+      "Maybe this Friday",
+      "Amma kitta keknu",
+      "Anytime 😏"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Saptiya?",
+    answer: "Friday velila kondu po",
+    options: [
+      "Sapte",
+      "Inu illa",
+      "Friday velila kondu po",
+      "Amma seiyonum"
+    ]
+  },
+];
